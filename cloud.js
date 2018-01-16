8dd6f529d45d4e73f63aef68daa2b3b2050573ef
Leon Fedotov <fedotov.leon@gmail.com>
2018-01-16 05:00:05 +0200

    touches

--- cloud.js
+++ cloud.js
@@ -1,8 +1,8 @@
 require('dotenv/config')
 const { isItPossible } = require('./lib')
 exports.isItPossible = function isItPossible (req, res) {
-	res.send(req.body)
-	// isItPossible(req.body.location).then(function(answer) {
-	// 	res.send('Hello World!'+answer)
-	// })
+    const location = 'broadway ny'
+    isItPossible(location ).then(function(answer) {
+        res.send('Hello World!'+answer)
+    })
 };
